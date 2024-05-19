97cdb28650c8999d773b2f54999c7c1ef6007924
Finney Osajere <osafinney21@gmail.com>
2024-05-19 03:35:04 +0100

    Revert "ALMOST"
    This reverts commit 84a247eea6b7efd886ed9310db7ac2784812b1da.

--- scripts/scheduler.js
+++
@@ -1,8 +0,0 @@
-import { schedule } from 'node-cron';
-import { sendBirthdayMessages } from './bot.js';
-
-// Schedule the function to run daily at midnight
-schedule('0 8 * * *', () => {
-  sendBirthdayMessages();
-  console.log('Scheduled task executed.');
-});
